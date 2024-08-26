71d53571a16eea5e81373b6061757e4a99edd5be
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-08-27 02:48:54 +0530

    feat: loading screen on initial load

--- src/Pages/AboutUs/AboutUs.tsx
+++ src/Pages/AboutUs/AboutUs.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaStar, FaLock, FaShippingFast, FaHeadset } from "react-icons/fa";
 import aboutUsImage from "../../Assets/Images/about.jpg";
@@ -33,23 +33,26 @@ const features = [
 ];
 
 const AboutUs: React.FC = () => {
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+
   return (
     <div>
       <Navbar />
 
       <header
-        className="relative h-64 flex items-center justify-center text-white
-      bg-teal-500 overflow-hidden
-      "
+        className="relative h-44 xs:h-40 lg:h-52 flex items-center justify-center text-white
+      bg-teal-500 overflow-hidden"
       >
         <div className="relative z-10 text-center px-4">
-          <h1 className="text-3xl sm:text-4xl font-bold">Discover Vendio</h1>
-          <p className="text-lg sm:text-xl mt-2">
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold">
+            Discover Vendio
+          </h1>
+          <p className="text-base sm:text-xl mt-2">
             Your Go-To Destination for Seamless Online Shopping
           </p>
           <Link
             to="/products"
-            className="mt-4 inline-block bg-white text-teal-500 font-semibold py-2 px-6 rounded-full transition-colors hover:bg-gray-100 shadow-lg"
+            className="text-sm lg:text-base mt-4 inline-block bg-white text-teal-500 font-semibold py-2 px-6 rounded-full transition-colors hover:bg-gray-100 shadow-lg"
           >
             Shop Now
           </Link>
@@ -59,18 +62,27 @@ const AboutUs: React.FC = () => {
       {/* Our Story Section */}
       <section className="py-8 px-4 md:px-8 lg:px-16 bg-white">
         <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center">
-          <div className="hidden md:flex md:w-1/2">
-            <img
-              src={aboutUsImage}
-              alt="Our Story"
-              className="rounded-lg shadow-lg w-full"
-            />
+          <div className="flex flex-col md:flex-row md:w-1/2">
+            <div
+              className={`rounded-lg shadow-lg w-auto h-[30dvh] md:h-[22rem] overflow-hidden ${
+                imageLoaded ? "bg-transparent" : "bg-gray-200 animate-pulse"
+              }`}
+            >
+              <img
+                src={aboutUsImage}
+                alt="Our Story"
+                className={`rounded-lg shadow-lg w-full h-full transition-opacity duration-300 object-cover ${
+                  imageLoaded ? "opacity-100" : "opacity-0"
+                }`}
+                onLoad={() => setImageLoaded(true)}
+              />
+            </div>
           </div>
           <div className="md:w-1/2 md:pl-8 mt-6 md:mt-0">
             <h2 className="text-2xl text-center font-bold text-gray-900">
               Our Journey
             </h2>
-            <p className="text-gray-700 mt-3 leading-relaxed sm:text-lg">
+            <p className="text-gray-700 mt-3 leading-relaxed sm:text-lg text-justify md:text-left">
               Vendio started with a simple idea: to make online shopping easier,
               faster, and more enjoyable for everyone. From our humble
               beginnings as a small startup, we've grown into a trusted
@@ -113,7 +125,7 @@ const AboutUs: React.FC = () => {
           <h2 className="text-2xl font-bold text-gray-900">
             Our Mission & Vision
           </h2>
-          <p className="text-gray-700 mt-3 max-w-4xl mx-auto leading-relaxed">
+          <p className="text-gray-700 mt-3 max-w-4xl mx-auto leading-relaxed text-justify md:text-center">
             Our mission is to provide an unparalleled online shopping experience
             by offering a vast selection of products, exceptional customer
             service, and innovative solutions. We envision becoming the leading
@@ -131,7 +143,7 @@ const AboutUs: React.FC = () => {
           </h2>
           <Link
             to="/products"
-            className="mt-4 inline-block bg-white text-teal-500 font-semibold py-2 px-6 rounded-full transition-colors hover:bg-gray-100 shadow-lg"
+            className="text-sm sm:text-base mt-4 inline-block bg-white text-teal-500 font-semibold py-2 px-6 rounded-full transition-colors hover:bg-gray-100 shadow-lg"
           >
             Start Shopping Now
           </Link>
--- src/Pages/Loading/Loading.tsx
+++ src/Pages/Loading/Loading.tsx
@@ -1,9 +1,16 @@
-const Loading = () => {
+import React from "react";
+
+const LoadingPage: React.FC = () => {
   return (
-    <div className="w-screen h-screen bg-pink-400 flex justify-center items-center">
-      <div className="max-w-sm h-auto text-2xl">Loading...</div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-teal-50 to-blue-50">
+      <div className="flex flex-col items-center">
+        <div className="w-16 h-16 border-t-4 border-b-4 border-black rounded-full animate-spin"></div>
+        <p className="mt-6 text-xl font-semibold text-black animate-pulse">
+          Loading, please wait...
+        </p>
+      </div>
     </div>
   );
 };
 
-export default Loading;
+export default LoadingPage;
