0ad70970115c364148c32aee1cdd021a113abc3c
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-09-22 21:26:14 +0530

    feat: search bar in navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,24 +1,25 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { CiLogin, CiLogout } from "react-icons/ci";
+import { CiLogin, CiLogout, CiSearch } from "react-icons/ci";
+import { LiaShoppingCartSolid } from "react-icons/lia";
+import { useEffect, useState } from "react";
 import logo from "../Assets/Images/logo.png";
 import defaultuser from "../Assets/Images/defaultuser.png";
+import NavbarMobile from "./NavbarMobile";
 import { useAppContext } from "../Context/AppContext";
 import { useCart } from "../Context/CartContext";
-import NavbarMobile from "./NavbarMobile";
-import { LiaShoppingCartSolid } from "react-icons/lia";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   const { isAuthenticated, user, logout } = useAppContext();
   const { cartItems } = useCart();
   const location = useLocation();
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const isLoginPage = location.pathname === "/login";
   const redirectUrl =
     new URLSearchParams(location.search).get("redirect") || "/";
 
   const navlinks = [
-    { title: "Home", path: "/" },
     { title: "About Us", path: "/about" },
     { title: "Products", path: "/products" },
   ];
@@ -27,11 +28,29 @@ const Navbar = () => {
     navigate("/account/profile");
   };
 
+  // Handle search submission
+  const handleSearchSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchQuery.trim()) {
+      navigate(`/products?query=${searchQuery.trim()}`);
+    }
+  };
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const query = queryParams.get("query");
+    if (query) {
+      setSearchQuery(query);
+    } else {
+      setSearchQuery("");
+    }
+  }, [location.search]);
+
   return (
     <>
-      <nav className="hidden md:flex w-full h-16 p-3 bg-white items-center justify-between px-8 lg:px-20 border-b border-b-stone-200 sticky top-0 z-50">
-        {/* Logo */}
-        <Link to="/" className="text-2xl font-bold flex gap-2 items-center">
+      <nav className="hidden md:flex w-full h-16 bg-white items-center justify-between px-6 xl:px-16 2xl:px-20 border-b border-b-stone-200 sticky top-0 z-50">
+        {/* Logo Section */}
+        <Link to="/" className="flex items-center gap-2 text-2xl font-bold">
           <img src={logo} alt="Vendio" className="w-8 h-8" />
           <div className="relative">
             <span className="text-gray-800">Vendio</span>
@@ -42,36 +61,69 @@ const Navbar = () => {
         </Link>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex items-center gap-6 lg:gap-10">
-          {navlinks.map((link) => (
-            <Link
-              to={link.path}
-              key={link.title}
-              className={`flex items-center text-gray-600 font-medium 
+        <div className="hidden md:flex items-center xl:gap-3 2xl:gap-6 ml-4">
+          <Link
+            to={"/about"}
+            key={"About Us"}
+            className={`hidden lg:flex items-center text-gray-600 font-medium 
               ${
-                location.pathname === link.path
+                location.pathname === "/about"
                   ? "text-teal-700 border-b-2 border-teal-600"
                   : "hover:text-teal-600"
               }
-              py-2 px-4 transition-colors duration-300 ease-in-out rounded-md
-              `}
-            >
-              {link.title}
-            </Link>
-          ))}
+              p-2 transition-colors duration-300 ease-in-out rounded-md`}
+          >
+            About Us
+          </Link>
+
+          <Link
+            to={"/products"}
+            key={"Products"}
+            className={`flex items-center text-gray-600 font-medium 
+               ${
+                 location.pathname === "/products"
+                   ? "text-teal-700 border-b-2 border-teal-600"
+                   : "hover:text-teal-600"
+               }
+              p-2 transition-colors duration-300 ease-in-out rounded-md`}
+          >
+            Products
+          </Link>
         </div>
 
-        {/* User Section and Cart */}
-        <div className="flex items-center gap-4 lg:gap-8">
+        {/* Search Bar */}
+        <form
+          onSubmit={handleSearchSubmit}
+          className="flex-grow max-w-sm xl:max-w-md mx-4"
+        >
+          <div className="relative">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search for products..."
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
+            />
+            <button
+              type="submit"
+              className="absolute top-1/2 transform -translate-y-1/2 right-2 text-teal-600 hover:text-teal-800"
+            >
+              <CiSearch className="text-xl" />
+            </button>
+          </div>
+        </form>
+
+        {/* User and Cart Section */}
+        <div className="flex items-center gap-4">
           <div
             className="flex items-center gap-2 text-sm font-semibold text-gray-600 cursor-pointer hover:text-gray-900 transition-colors duration-300"
             title="account"
             onClick={handleProfileClick}
           >
-            <span className="text-gray-800 max-w-40 truncate">
-              {isAuthenticated ? `Hello, ${user?.firstName}` : "Hi, Guest"}
+            <span className="text-gray-800 max-w-20 md:max-w-24 lg:max-w-48 truncate">
+              {isAuthenticated ? `Hi, ${user?.firstName}` : "Hi, Guest"}
             </span>
-            <div className="bg-gray-100 rounded-full overflow-hidden">
+            <div className="bg-gray-100 rounded-full w-8">
               <img
                 src={user?.profilePic || defaultuser}
                 alt="Profile"
